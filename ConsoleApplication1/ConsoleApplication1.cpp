eabe52ccfe49cc2a8e74bf82eef090608cc28832
Сидоренко Александр <92943758+Kotbert@users.noreply.github.com>
2023-02-15 20:17:31 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+    string inputString = "Success";
+    string repeatList = "";
+    string result = "";
+    string indexedString = "";
+    for (char c : inputString) {
+        indexedString += tolower(c);
+    }
+    for (int i = 0; i < indexedString.length(); i++) {
+        char someVal = indexedString[i];
+        if (repeatList.find(someVal) != string::npos) {
+            result += ')';
+        }
+        else {
+            for (int y = i; y < indexedString.length(); y++) {
+                if (y == indexedString.length() - 1) {
+                    result += '(';
+                }
+                if (y != i && someVal == indexedString[y]) {
+                    repeatList += indexedString[i];
+                    result += ')';
+                    break;
+                }
+            }
+        }
+    }
+    cout << result << endl;
+    return 0;
+}
